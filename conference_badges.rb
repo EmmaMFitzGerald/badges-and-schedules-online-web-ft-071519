833f1038b623672ddbcf3c7c1e24c6a9e1a01d3c
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-15 16:53:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ end
 
 def assign_rooms(speakers)
   new_array = []
-  array.each_with_index do |name, index|
+  speakers.each_with_index do |name, index|
   new_array << "Hello #{name}! You'll be assigned to room #{index + 1}" 
   end
   new_array
