a87af6cc24c0a2eab9ffbbd580d1c611b204b398
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-15 17:01:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,11 +18,8 @@ def assign_rooms(speakers)
   return new_array
 end
 
-def printer(name)
- batch_badge_creator.each do |x|
-   puts x 
- end
- assign_rooms.each do |y|
-   puts y 
- end
+def printer(attendees)
+#print the name of each index in the array
+   batch_badge_creator(attendees).each { |names| puts names }
+   assign_rooms(attendees).each { |room| puts room }
 end
